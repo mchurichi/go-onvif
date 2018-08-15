11f6cfa508897c299cd9e9cc0e6ce4bc6d1eed7c
Maximiliano Churichi <mchurichi@gmail.com>
2018-08-14 22:59:57 -0300

    Fix uuid

--- discovery.go
+++ discovery.go
@@ -48,7 +48,7 @@ func StartDiscovery(duration time.Duration) ([]Device, error) {
 
 func discoverDevices(ipAddr string, duration time.Duration) ([]Device, error) {
 	// Create WS-Discovery request
-	requestID := "uuid:" + uuid.NewV4().String()
+	requestID := "uuid:" + uuid.Must(uuid.NewV4()).String()
 	request := `		
 		<?xml version="1.0" encoding="UTF-8"?>
 		<e:Envelope
--- soap.go
+++ soap.go
@@ -105,7 +105,7 @@ func (soap SOAP) createRequest() string {
 }
 
 func (soap SOAP) createUserToken() string {
-	nonce := uuid.NewV4().Bytes()
+	nonce := uuid.Must(uuid.NewV4()).Bytes()
 	nonce64 := base64.StdEncoding.EncodeToString(nonce)
 	timestamp := time.Now().Add(soap.TokenAge).UTC().Format(time.RFC3339)
 	token := string(nonce) + timestamp + soap.Password
